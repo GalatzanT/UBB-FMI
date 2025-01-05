af064a4dbc758b75d987f9f1dc05afe36f6cae51
Galatan Tudor <150925784+GalatzanT@users.noreply.github.com>
2025-01-05 23:56:28 +0200

    Add files via upload

---
+++ Semestru 3/MAP/lab7/lab7.sql
@@ -0,0 +1,44 @@
+CREATE TABLE Utilizatori (
+    id SERIAL PRIMARY KEY,
+    nume VARCHAR(100),
+    prenume VARCHAR(100),
+    email VARCHAR(100),
+    password VARCHAR(100)
+);
+
+
+CREATE TABLE Prietenii (
+    id SERIAL PRIMARY KEY,
+    id_user1 BIGINT NOT NULL,
+    id_user2 BIGINT NOT NULL,
+    friends_from TIMESTAMP NOT NULL,
+    CONSTRAINT fk_user1 FOREIGN KEY (id_user1) REFERENCES Utilizatori (id) ON DELETE CASCADE,
+    CONSTRAINT fk_user2 FOREIGN KEY (id_user2) REFERENCES Utilizatori (id) ON DELETE CASCADE,
+    CONSTRAINT unique_friendship UNIQUE (id_user1, id_user2),
+    CONSTRAINT no_self_friendship CHECK (id_user1 <> id_user2)
+);
+
+
+create table cereri
+(
+	id serial primary key,
+	id_user1 BIGINT NOT NULL,
+	id_user2 bigint not null,
+	time timestamp default CURRENT_TIMESTAMP not null,
+	status varchar(100),
+	constraint fk_user1 foreign key (id_user1) references Utilizatori(id) on delete cascade,
+	constraint fk_user2 foreign key (id_user2) references Utilizatori(id) on delete cascade,
+	constraint unigue_request unique (id_user1,id_user2)
+);	
+
+create table mesaje (
+	id serial primary key,
+	id_user1 bigint not null,
+	id_user2 bigint not null,
+	text VARCHAR(100),
+	timp timestamp not null,
+	raspuns_la bigint references mesaje(id),
+	Constraint fk_user1 foreign key(id_user1) references utilizatori(id) on delete cascade,
+	constraint fk_user2 foreign key(id_user2) references utilizatori(id) on delete cascade,
+	constraint no_self_mesaje check(id_user1 <> id_user2)
+);
